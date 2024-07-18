58ac50106cd5d60a5352b49c98c60968eedb6543
VIA2NEY <kouameangeV11@gmail.com>
2024-07-18 22:14:32 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/day_3_fodd_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day_3_fodd_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:day_3_fodd_delivery/pages/starter_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      home: StarterPage(),
+    );
+  }
+}
